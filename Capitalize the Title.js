c3011f638807dbb3e9a28f95d06ff1a6b6ad9ae2
HOrxan <54819116+HOrxan@users.noreply.github.com>
2026-03-09 14:04:49 +0400

    4

---
+++ Capitalize the Title.js
@@ -0,0 +1,14 @@
+  function solution(title) {
+    // Write your code here
+     let a = title.toLowerCase();
+        let b= a.split(" "); 
+        let c, k, s, t, y="";
+        for (let i = 0; i < b.length; i++) {
+            c = b[i];
+            k = c[0]
+            s = k.toUpperCase();
+            t = c.slice(1, c.length);
+        y += s + t + " ";
+        }
+    return y;
+}
