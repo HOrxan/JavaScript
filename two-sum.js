45908df023c25154ece50e89faf5c80dd586a7e9
HOrxan <54819116+HOrxan@users.noreply.github.com>
2026-03-18 22:40:12 +0400

    1

--- two-sum.js
+++ two-sum.js
@@ -1,19 +1,6 @@
-const fs = require('fs');
-
-const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
-
-function solution(nums, target) {
+function solution() {
     // Write your code here
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) return [i, j];
-        }
-    }
+    return document.getElementById("demo").innerHTML = add(1, 7);
     
-    return []; // Added return statement for cases where no solution is found
 }
-
-const nums = JSON.parse(lines[0]);
-const target = parseInt(lines[1]);
-
-console.log(JSON.stringify(solution(nums, target)));
+console.log(solution());
