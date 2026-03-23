e641b9c970cec041bbc6932c463403d8d3b140b7
HOrxan <54819116+HOrxan@users.noreply.github.com>
2026-03-23 23:23:23 +0400

    8

--- check-if-two-chessboard-squares-have-the-same-color.js
+++ check-if-two-chessboard-squares-have-the-same-color.js
@@ -3,7 +3,6 @@ const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 
 function solution(coordinate1, coordinate2) {
     // Write your code here
-
     if ((coordinate1[0].charCodeAt(0) - 96 + Number(coordinate1[1])) % 2 == (coordinate2[0].charCodeAt(0) - 96 + Number(coordinate2[1])) % 2) { return true }
     else { return false }
 }
